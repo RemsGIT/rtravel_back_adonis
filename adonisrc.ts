2a9c05c4c27cbdc87c1c90c6167ca9e60de899fc
Remy CASTRO <remycastro27@icloud.com>
2024-05-23 15:55:48 +0200

    Remove older upload folder

--- adonisrc.ts
+++ adonisrc.ts
@@ -77,10 +77,6 @@ export default defineConfig({
   },
   metaFiles: [
     {
-      pattern: 'public/**',
-      reloadServer: false,
-    },
-    {
       pattern: 'resources/views/**/*.edge',
       reloadServer: false,
     },
