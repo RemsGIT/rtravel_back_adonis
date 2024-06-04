cd5bcf387602af3ad9a29d8b784d1319f92043ac
Remy CASTRO <remycastro27@icloud.com>
2024-06-04 16:57:33 +0200

    Edit participant

--- app/controllers/participants_controller.ts
+++ app/controllers/participants_controller.ts
@@ -3,6 +3,7 @@
 import { HttpContext } from '@adonisjs/core/http'
 import { createParticipantValidator, updateParticipantValidator } from '#validators/participant'
 import Participant from '#models/participant'
+import User from '#models/user'
 
 export default class ParticipantsController {
   async store({ params, request, response }: HttpContext) {
@@ -18,6 +19,12 @@ export default class ParticipantsController {
       if (isParticipantInTrip) {
         return response.abort({ error: 'ALREADY_EXISTS' })
       }
+    }
+
+    // Check if the email is an account
+    const hasAccount = await this.checkIfParticipantHasAnAccount(payload)
+    if (hasAccount) {
+      payload.name = hasAccount
     }
 
     const participant = await Participant.create({
@@ -38,17 +45,29 @@ export default class ParticipantsController {
 
   async update({ params, request, response }: HttpContext) {
     const participant = await Participant.findOrFail(params.id)
-
     const payload = await request.validateUsing(updateParticipantValidator)
 
-    const participantUpdated = await participant
-      .merge({
-        name: payload.name,
-        email: payload.email,
-        tripId: Number(params.tripId),
-        policy: payload.policy,
-      })
-      .save()
+    payload.email = payload.email ?? ''
+
+    // Check if participant isn't already in the trip -> if new email
+    if (payload.email !== participant.email) {
+      const isParticipantInTrip = await Participant.query()
+        .where('email', payload.email)
+        .andWhere('tripId', Number(params.tripId))
+        .first()
+
+      if (isParticipantInTrip) {
+        return response.abort({ error: 'ALREADY_EXISTS' })
+      }
+    }
+
+    // Check if the email is an account
+    const hasAccount = await this.checkIfParticipantHasAnAccount(payload)
+    if (hasAccount) {
+      payload.name = hasAccount
+    }
+
+    const participantUpdated = await participant.merge(payload).save()
 
     return response.ok(participantUpdated)
   }
@@ -59,5 +78,11 @@ export default class ParticipantsController {
     await participant.delete()
 
     return response.ok({ message: `Participant ${participant.id} deleted` })
+  }
+
+  private async checkIfParticipantHasAnAccount(payload: any): Promise<string | null> {
+    const user = await User.query().where('email', payload.email ?? '')
+
+    return user.length > 0 ? user[0].username : null
   }
 }
